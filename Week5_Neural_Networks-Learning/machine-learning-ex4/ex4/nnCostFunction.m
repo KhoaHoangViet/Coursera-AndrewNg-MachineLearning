de0c2178d81a32f384fbf53c4cb2238b934b0c68
tardigrades <dothisnows@gmail.com>
2018-02-26 01:28:54 +0700

    Assignment: Cost function without regularization

--- Week5_Neural_Networks-Learning/machine-learning-ex4/ex4/nnCostFunction.m
+++ Week5_Neural_Networks-Learning/machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,12 +62,23 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+a1 = [ones(m, 1) X]; % Dimension = [m x (s1+1)]
 
+a2 = sigmoid(a1*Theta1');
+a2 = [ones(m, 1) a2]; % Dimension = [m x (s2+1)]
 
+a3 = sigmoid(a2*Theta2'); % Dimension = [m * K]
+hypothesis = a3;
 
+% Logical vector for y
+yv=[1:num_labels] == y; %  Dimension = [m * K]
 
-
-
+%% Cost function without regularization
+temp = -yv.*log(hypothesis) - (1-yv).*log(1-hypothesis); %dimension = [m * K]
+% Sum as K(sum all elements of each row)
+temp = sum(temp,2); % Dimension = [m * 1]
+% Sum as m(sum all element of each column)
+J = 1/m*sum(temp); % Dimension = [1 * 1]
 
 
 
